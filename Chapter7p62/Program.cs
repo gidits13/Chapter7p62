e1e01acc31d5e3bda24356037084b2e2da68755e
gidits13 <gidits13@gmail.com>
2022-08-25 19:02:34 +0300

    Добавьте файлы проекта.

---
+++ Chapter7p62/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Chapter7p62
+{
+    class Car<T>
+    {
+        public T Engine;
+    }
+    class  ElectricEngine
+        {}
+    class GasEngine
+    { }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

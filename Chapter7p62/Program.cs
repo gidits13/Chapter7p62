33ff38415eca744f2f25044337733d2d7937661e
gidits13 <gidits13@gmail.com>
2022-08-25 19:56:42 +0300

    Задание 7.6.7

--- Chapter7p62/Program.cs
+++ Chapter7p62/Program.cs
@@ -1,13 +1,23 @@
 ﻿namespace Chapter7p62
 {
-    class Car<T>
+    class Car<T1>
     {
-        public T Engine;
+        public T1 Engine;
+        public virtual void ChangePart<T2>(T2 newPart)
+        { }
     }
     class  ElectricEngine
         {}
     class GasEngine
     { }
+    
+    class Battery
+    { }
+
+    class Differential
+    { }
+
+    class Wheel
 
     internal class Program
     {

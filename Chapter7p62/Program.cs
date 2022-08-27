ba7b9f163691fd81b26498b60bddabccfad9d466
gidits13 <gidits13@gmail.com>
2022-08-27 15:37:03 +0300

    Задание 7.6.12

--- Chapter7p62/Program.cs
+++ Chapter7p62/Program.cs
@@ -4,11 +4,25 @@
         {}
     abstract public class CarPart
     { }
-    class Car<TEngine> where TEngine: Engine
+    abstract class Car<TEngine> where TEngine: Engine
     {
         public TEngine Engine;
         public virtual void ChangePart<TPart>(TPart newPart) where TPart : CarPart
         { }
+    }
+    class ElectricCar: Car<ElectricEngine>
+    {
+        public override void ChangePart<TPart>(TPart newPart)
+        {
+            
+        }
+    }
+    class GasCar:Car<GasEngine>
+    {
+        public override void ChangePart<TPart>(TPart newPart)
+        {
+            
+        }
     }
     class  ElectricEngine: Engine
         {}
